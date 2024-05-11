f0fd4f22fe3a246258fa73741671731ee0b17613
SIVASURYA <112889318+sivasurya01@users.noreply.github.com>
2024-05-11 04:06:53 -0700

    Update projects.tsx

--- components/projects.tsx
+++ components/projects.tsx
@@ -12,26 +12,26 @@ export default function Projects() {
 
   return (
     <>
-    <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
-      <SectionHeading>My projects</SectionHeading>
-      <div>
-        {projectsData.map((project, index) => (
-          <React.Fragment key={index}>
-            <Project {...project} />
-          </React.Fragment>
-        ))}
-      </div>
-    </section>
-    <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
-      <SectionHeading>Mini projects</SectionHeading>
-      <div>
-        {miniprojectData.map((project, index) => (
-          <React.Fragment key={index}>
-            <Project {...project} />
-          </React.Fragment>
-        ))}
-      </div>
-    </section>
+      <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
+        <SectionHeading>My projects</SectionHeading>
+        <div>
+          {projectsData.map((project, index) => (
+            <React.Fragment key={index}>
+              <Project {...project} />
+            </React.Fragment>
+          ))}
+        </div>
+      </section>
+      <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
+        <SectionHeading>Own/Mini projects</SectionHeading>
+        <div>
+          {miniprojectData.map((project, index) => (
+            <React.Fragment key={index}>
+              <Project {...project} />
+            </React.Fragment>
+          ))}
+        </div>
+      </section>
     </>
   );
 }
